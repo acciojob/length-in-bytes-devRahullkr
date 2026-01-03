86643d53a4713b6bce3bc70521f87f9e0f1b06e6
Rahul Kr <rahulkumar8604745@gmail.com>
2026-01-03 23:28:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const byteSize = (str) => {
   // write your code here
+return new TextEncoder().encode(str).length;
+	
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.");
+const str = prompt("hello world");
 alert(byteSize(str));
